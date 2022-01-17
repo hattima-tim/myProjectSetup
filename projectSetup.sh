5f20ecca95db572b06df357a79efa9350c8b5805
ridwan <irfanridwan20@gmail.com>
2022-01-17 23:07:57 +0600

    initial commit

---
+++ projectSetup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+npm init -y
+npm install webpack webpack-cli eslint@6.8.0 --save-dev
+npx eslint --init
+mkdir dist src
+cd dist
+touch index.html
+cd ..
+cd src
+touch index.js
+cd ..
+touch webpack.config.js
+
+printf "const path = require('path'); \n
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};" > webpack.config.js
+
+#Set execute permission on your script using chmod command :
+#chmod 755 script-name-here.sh
