0e26c017e8eddd689ddbba5c8ed765b139af91ce
ridwan <irfanridwan20@gmail.com>
2022-01-17 23:40:23 +0600

    added a command to address problems related to the installing packeges via npm

--- projectSetup.sh
+++ projectSetup.sh
@@ -4,6 +4,9 @@ set -x
 cd ..
 npm init -y
 npm install webpack webpack-cli eslint@6.8.0 --save-dev
+npm audit fix --force
+#line 7 is here because npm recommended to use the 
+#command to address all the issues(including breaking changes).Maybe they are recommending it because I am using an older version of Node?
 npx eslint --init
 mkdir dist src
 cd dist
